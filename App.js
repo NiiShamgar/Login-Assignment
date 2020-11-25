61aa508765d59a8e4b1b55ea08fcd6fb38c424ea
unknown <niiaddoac@gmail.comgit config --global user.name NiiShamgar>
2020-11-25 12:11:58 +0000

    first commit- the transfer

--- App.js
+++ App.js
@@ -1,21 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView, FlatList, TouchableNativeFeedback}  from 'react-native';
+import LogIn from './LogIn'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      email:"",
+      password:"",
+      names:["ama","Adjoa","Kwame", "Kofi","ama","Adjoa","Kwame", "Kofi","ama","Adjoa","Kwame", "Kofi","ama","Adjoa","Kwame", "Kofi","ama","Adjoa","Kwame", "Kofi","ama","Adjoa","Kwame", "Kofi","ama","Adjoa","Kwame", "Kofi","ama","Adjoa","Kwame", "Kofi","Kofi","ama","Adjoa","Kwame", "Kofi","ama","Adjoa","Kwame", "Kofi","ama","Adjoa","Kwame", "Kofi","ama","Adjoa","Kwame", "Kofi","ama","Adjoa","Kwame", "Kofi","ama","Adjoa","Kwame", "Kofi"]
+    }
+  } 
+  render(){
+    return (
+      <View style={styles.container}>
+          <LogIn/>
+
+
+         <TextInput 
+            style={styles.input} 
+            placeholder="email"
+            autoCapitalize="none"
+            autoCorrect={false}
+            //secureTextEntry={true}
+            value={this.state.email}
+            onChangeText={(email)=>{
+              this.setState({email})
+           }}
+            />
+
+
+        <TextInput 
+            style={styles.input}
+            placeholder="password"
+            autoCapitalize="none"
+            autoCorrect={false}
+            secureTextEntry={true}
+            value={this.state.password}
+            onChangeText={(password)=>{
+              this.setState({password})
+            }}
+            />
+
+            <TouchableOpacity style={styles.loginContainer}>
+              <Text style={styles.loginText}>Login</Text>
+          </TouchableOpacity>  
+          
+      </View>
+    );   
+  } 
 }
 
+
+ 
+ 
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+     marginTop:100,
+     marginHorizontal:20
   },
+  input:{
+    marginVertical:10,
+    paddingHorizontal:10,
+    backgroundColor:"green",
+    height:50
+  },
+  loginContainer:{
+    backgroundColor:"blue",
+    height:40,
+    justifyContent:"center",
+    borderRadius:20,
+    marginHorizontal:20
+
+  },
+  loginText:{
+    
+    color:"white",
+    alignSelf:"center"
+  }
 });
+
+export default App; 
+
+ 
